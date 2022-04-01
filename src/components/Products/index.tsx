92e8ab3f38bb576e6ddacf878c0fdb3cf00c60c2
isaacviannadev <isaacsvianna@gmail.com>
2022-04-01 11:52:28 -0300

    fix import imgs

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -6,7 +6,7 @@ const itens = [
   {
     id: 0,
     name: 'Qualidade',
-    bgImage: '/images/main.png',
+    bgImage: '/Images/main.png',
     description: `Chegamos com a missão de trazer
 muito mais que um mercado, 
 trazemos pizzas, pratos executivos,
@@ -17,7 +17,7 @@ mais para você e sua família.
   {
     id: 1,
     name: 'Café da manhã',
-    bgImage: '/images/cafemanha.png',
+    bgImage: '/Images/cafemanha.png',
     description: `Temos um CAFÉ DA MANHÃ completo para você e sua família com muito sabor e variedade, são muitas opções com diversos 
 tipos de pães, sucos da fruta, laticínios, frios, bolos, ovos e muito mais!
   `,
@@ -25,67 +25,67 @@ tipos de pães, sucos da fruta, laticínios, frios, bolos, ovos e muito mais!
   {
     id: 2,
     name: 'Produtos Orgânicos',
-    bgImage: '/images/organicos.png',
+    bgImage: '/Images/organicos.png',
     description: `Você pode contar com muitos produtos ORGÂNICOS para o seu dia a dia, temos uma área especial para você! `,
   },
   {
     id: 3,
     name: 'Cortes especiais',
-    bgImage: '/images/acougue.png',
+    bgImage: '/Images/acougue.png',
     description: `Em nosso AÇOUGUE você vai encontrar cortes nobres e as melhores opções para o 
 seu dia a dia.`,
   },
   {
     id: 4,
     name: 'Peixe fresco',
-    bgImage: '/images/peixe.png',
+    bgImage: '/Images/peixe.png',
     description: `PEIXE FRESCO todo dia! Uma seleção de frutos  do mar com camarão vg, salmão, polvo, lula, 
 variados tipos de peixe, tudo com opções já porcionadas pensando em você!`,
   },
   {
     id: 5,
     name: 'Hortifruti',
-    bgImage: '/images/hortifruti.png',
+    bgImage: '/Images/hortifruti.png',
     description: `Aqui no nosso HORTIFRUTI você vai encontrar muita qualidade e variedade alem de opções já porcionadas para o seu dia a dia, podemos fazer do seu jeito combinando legumes e verduras à sua escolha.`,
   },
   {
     id: 6,
     name: 'Vegano',
-    bgImage: '/images/vegano.png',
+    bgImage: '/Images/vegano.png',
     description: `Você pode se deliciar com nossas opções VEGANAS, temos lasanhas, pizza, risoto, gnocchi, bruchetta e muito mais.  `,
   },
   {
     id: 7,
     name: 'Pizza',
-    bgImage: '/images/pizza.png',
+    bgImage: '/Images/pizza.png',
     description: `Nossas PIZZAS são produzidas aqui mesmo e com um processo de fermentação natural,
 são varios sabores aguardando você! `,
   },
   {
     id: 8,
     name: 'Mercearia',
-    bgImage: '/images/mercearia.png',
+    bgImage: '/Images/mercearia.png',
     description:
       'Na nossa MERCEARIA você encontra s eus produtos favoritos e muitos outros para você se deliciar!',
   },
   {
     id: 9,
     name: 'Laticínios',
-    bgImage: '/images/queijo.png',
+    bgImage: '/Images/queijo.png',
     description:
       'Em nosso setor de LATICÍNIOS temos uma variedade de produtos para completar e deixar ainda mais gostoso seu café da manhã, jantar especial ou o lanchinho das crianças!',
   },
   {
     id: 10,
     name: 'Adega',
-    bgImage: '/images/adega.png',
+    bgImage: '/Images/adega.png',
     description:
       'Uma ADEGA completa com os melhores rótulos de vinho, além de licores e espumantes .',
   },
   {
     id: 11,
     name: 'Padaria',
-    bgImage: '/images/padaria.png',
+    bgImage: '/Images/padaria.png',
     description:
       'Uma PADARIA completa com muita coisa gostosa, você pode contar com pães de fermentação natural, bombas de chocolate, pães doces, pão de queijo e muitas outras opções!',
   },
